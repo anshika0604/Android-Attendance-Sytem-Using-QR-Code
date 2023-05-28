a66f65ec24756fc846c9bea9ae78e22bb3812dfb
Anshika <anshikanegi.cs19@gmail.com>
2023-05-28 18:29:53 +0530

    updated

---
+++ app/src/main/java/com/example/attendancesystemusingqr/Faculty_loginpage.java
@@ -0,0 +1,14 @@
+package com.example.attendancesystemusingqr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Faculty_loginpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faculty_loginpage);
+    }
+}
