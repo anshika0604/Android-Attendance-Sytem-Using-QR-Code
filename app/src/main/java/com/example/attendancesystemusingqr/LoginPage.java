85e6ff029a3e5941a1d4b70db56fdae5ae568bfc
Anshika <anshikanegi.cs19@gmail.com>
2023-05-30 00:35:40 +0530

    updated

--- app/src/main/java/com/example/attendancesystemusingqr/LoginPage.java
+++ app/src/main/java/com/example/attendancesystemusingqr/LoginPage.java
@@ -1,6 +1,7 @@
 package com.example.attendancesystemusingqr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.ActionBar;
 import android.content.Intent;
@@ -8,12 +9,18 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
+
 public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_button_foreground);
 
     }
     public void student_register(View view) {
